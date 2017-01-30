02d216baf938f7a507375324cdf998b7b32c4a67
Hossein <m.h.askari.hemmat@gmail.com>
2017-01-29 21:15:18 -0500

    adding support for iverilog

--- GCD/do_test.cmd
+++ GCD/do_test.cmd
@@ -1,4 +1,28 @@
 #!/bin/csh -f
+set IRUN_EXIST=0
+set IVERILOG_EXIST=0
+set SIM=""
+
+which irun >& /dev/null
+if ($? != 1) then
+    echo "found irun on this system!"
+    set SIM="irun"
+    set IRUN_EXIST=1    
+endif
+
+which iverilog >& /dev/null
+if ($? != 1) then
+    echo "found iverilog on this system!"
+    set SIM="iverilog"
+    set IVERILOG_EXIST=1
+endif
+
+if ( ( $IRUN_EXIST == 0 ) && ( $IVERILOG_EXIST == 0 ) ) then
+    echo "Neither irun nor iverilog exist on this machine!"
+    echo "I give up"
+    exit(1)
+endif
+
 
 #Test case name
 if ($#argv >= 1) then
@@ -29,12 +53,20 @@ if ($#argv >= 1) then
         set flags = "$flags -f files.f"
     endif
     echo "flags = $flags"
-    irun +access+rwc $flags
+    if( $IVERILOG_EXIST == 1 ) then
+        echo "running test...   "
+        iverilog '-Wall' '-g2012' -f files.f && unbuffer vvp a.out 
+    else 
+        irun +access+rwc $flags
+    endif
 else
-    if ( -f files.f) then
+    if( $IVERILOG_EXIST == 1 ) then
+        echo "running test...   "
+        iverilog '-Wall' '-g2012' -f files.f && unbuffer vvp a.out 
+    else if ( $IRUN_EXIST == 1 ) 
         irun +access+rwc -f files.f
     else
-        echo "Usage ./do_test.cmd [Test Name] [args...]"
+        echo "No simulator was set to use!"
         exit(1)
     endif
 endif
